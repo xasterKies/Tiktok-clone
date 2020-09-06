e0b18024a25fc9254edcfb7dec356adce1780f87
samueltiokeng <samueledward40@gmail.com>
2020-09-06 13:30:02 +0200

    c

--- tik-tok-clone/src/App.js
+++ tik-tok-clone/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        Hello Clever Programmers
         <div className="app__videos"></div>
           <Video/>
           <Video/>
