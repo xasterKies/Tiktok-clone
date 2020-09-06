f7fff4c166356fd3ab30bdba9e7718403ce7ec86
samueltiokeng <samueledward40@gmail.com>
2020-09-06 17:13:22 +0200

    vf

--- tik-tok-clone/src/VideoFooter.js
+++ tik-tok-clone/src/VideoFooter.js
@@ -1,1 +1,12 @@
 import React from "react";
+import "./VideoFooter.css"; 
+
+function VideoFooter() {
+    return(
+        <div className="videoFooter">
+            <h2>I am a footer</h2>
+        </div>
+    );
+}
+
+export default VideoFooter;
