aa8435839dfa8bbe097521c041ff47b1fc4459e2
samueltiokeng <samueledward40@gmail.com>
2020-09-06 18:07:52 +0200

    v

--- tik-tok-clone/src/VideoFooter.js
+++ tik-tok-clone/src/VideoFooter.js
@@ -9,7 +9,7 @@ function VideoFooter() {
             <p>This is a description</p>
             </div>
         
-        <img src="https://static.thenounproject.com/png
+        <img className="videoFooter__logo" src="https://static.thenounproject.com/png
         /934821-200.png" alt=""/>
         </div>
     );
