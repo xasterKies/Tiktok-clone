68e38dbd59a24be5a17eeba9a2218de66f27e3e2
samueltiokeng <samueledward40@gmail.com>
2020-09-06 14:23:17 +0200

    c

--- tik-tok-clone/src/Video.js
+++ tik-tok-clone/src/Video.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./Video.css";
 
 function Video() {
-    return(
+    const videoRef = useRef(null);
+    
+
         <div className="video">
         <video className="video__player" 
         loop
+        onClick={onVideoPress}
+        ref={videoRef}
         src="https://vm.tiktok.com/ZSPgqUJf/" className="video"></video>
         {/* <VideoFooter /> */}
         {/* <VideoSidebar /> /> */}
         </div>
-    )
+    
 }
 
 export default Video;
