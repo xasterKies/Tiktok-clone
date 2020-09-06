a605b432059ad9571971c284098e7be0ad9783c3
samueltiokeng <samueledward40@gmail.com>
2020-09-06 10:59:56 +0200

    v

--- tik-tok-clone/src/Video.js
+++ tik-tok-clone/src/Video.js
@@ -3,11 +3,6 @@ import "./Video.css";
 
 function Video() {
     return{
-        <div className="video">
-        <h1>this is a video</h1>
-        {/* <VideoFooter/> */}
-        {/* <Videos/> */}
-
-        </div>
+        
     }
 }
