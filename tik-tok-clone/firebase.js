41d06d4db8ab746cdcc472735794d7c616c65c8f
samueltiokeng <samueledward40@gmail.com>
2020-09-06 06:51:16 +0200

    f

--- tik-tok-clone/firebase.js
+++ tik-tok-clone/firebase.js
@@ -1,11 +1,4 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.19.1/firebase-analytics.js"></script>
-
-<script>
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyDgUyrfFShw0ayMCbGIJysqMq43Vtbavk0",
@@ -20,4 +13,3 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-</script>
