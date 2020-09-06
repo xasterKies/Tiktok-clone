30e07adc63370e8c221480588dc2b2793eec4e5b
samueltiokeng <samueledward40@gmail.com>
2020-09-06 13:33:31 +0200

    clever

--- tik-tok-clone/src/App.js
+++ tik-tok-clone/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Clever Programmer</h1>
+        Clever Programmer
         <div className="app__videos">
           <Video/>
           <Video/>
