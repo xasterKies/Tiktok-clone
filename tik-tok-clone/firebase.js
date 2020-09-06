11705a32c2383ca88708b13321dfee67f56aaa06
samueltiokeng <samueledward40@gmail.com>
2020-09-06 06:50:13 +0200

    firebase

---
+++ tik-tok-clone/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.19.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDgUyrfFShw0ayMCbGIJysqMq43Vtbavk0",
+    authDomain: "tik-tok-clone-fe8f6.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-fe8f6.firebaseio.com",
+    projectId: "tik-tok-clone-fe8f6",
+    storageBucket: "tik-tok-clone-fe8f6.appspot.com",
+    messagingSenderId: "823381937089",
+    appId: "1:823381937089:web:a627230b83f6cd82e33d40",
+    measurementId: "G-04H3VK9VZ0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
