7c53b9d1523b1429d1d2489c7db9e2517c020634
samueltiokeng <samueledward40@gmail.com>
2020-09-11 02:56:41 +0200

    v

--- tik-tok-clone/src/Video.js
+++ tik-tok-clone/src/Video.js
@@ -21,7 +21,7 @@ function Video() {
                 loop
                 onClick={onVideoPress}
                 ref={videoRef}
-                src="https://www.tiktok.com/@larissadeffo/video/6866943467520609537?_d=secCgsIARCbDRgBIAMoARI%2BCjzC1oi3Yjr%2FE6rpJt3GxWACHieCVnLJOcV7F5esNpxqSfgtvLA3l5nErnxXZg%2F71JqyFOQzFopL%2FSidcIoaAA%3D%3D&language=en&preview_pb=0&sec_user_id=MS4wLjABAAAAdhX--l4iKv-ZZJKEY2TKNGmRIkEE0Gd_TJDY1BE-axWVIpH2WrpkYnK61vZr2ovR&share_item_id=6866943467520609537&share_link_id=C09B044D-3DE6-41AD-AA9A-AFEA329052C7&timestamp=1599372540&tt_from=copy&u_code=da4cdmmjfgfk30&user_id=6775952522306831366&utm_campaign=client_share&utm_medium=ios&utm_source=copy&source=h5_m" className="video">
+                src="https://www.tiktok.com/@larissadeffo/video/6866943467520609537?_d=secCgsIARCbDRgBIAMoARI%2BCjzC1oi3Yjr%2FE6rpJt3GxWACHieCVnLJOcV7F5esNpxqSfgtvLA3l5nErnxXZg%2F71JqyFOQzFopL%2FSidcIoaAA%3D%3D&language=en&preview_pb=0&sec_user_id=MS4wLjABAAAAdhX--l4iKv-ZZJKEY2TKNGmRIkEE0Gd_TJDY1BE-axWVIpH2WrpkYnK61vZr2ovR&share_item_id=6866943467520609537&share_link_id=C09B044D-3DE6-41AD-AA9A-AFEA329052C7&timestamp=1599372540&tt_from=copy&u_code=da4cdmmjfgfk30&user_id=6775952522306831366&utm_campaign=client_share&utm_medium=ios&utm_source=copy&source=h5_m">
             </video>
         <VideoFooter/>
 
